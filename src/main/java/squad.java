2f66e22dd673ff103c4da16964b09880c83d3871
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-19 00:31:09 +0300

    adding content to the success template resources

--- src/main/java/squad.java
+++ src/main/java/squad.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.Contract;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,48 @@ public class squad {
         mHeroes = new ArrayList<Hero>();
     }
 
-    public String getmName() {
+    public String getName() {
         return mName;
     }
+
+    public int getSize() {
+        return mSize;
+    }
+
+    public String getCause() {
+        return mCause;
+    }
+
+    public static List<squad> all() {
+        return instances;
+    }
+    public static void clear(){
+        instances.clear();
+    }
+    public int getId(){
+        return mId
+    }
+    @Contract(pure = true)
+    public static squad find(int id){
+        return instances.get(id - 1);
+    }
+    public List<Hero>getHeroes(){
+        return mHeroes;
+    }
+    public void addHero(Hero hero) {
+        mHeroes.add(hero);
+    }
+    public static boolean heroAlreadyExists(Hero newHero) {
+        boolean exists = false;
+        for (Squad squad : instances) {
+            for (Hero hero : squad.getHeroes()) {
+                if (hero.getName().equals(newHero.getName())) {
+                    exists = true;
+                }
+            }
+        }
+        return exists;
+    }
+
+
 }
