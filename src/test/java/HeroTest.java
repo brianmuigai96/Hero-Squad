2073b83e47c781e02a08ee0751ab8a9c5192385c
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-18 19:08:07 +0300

    creating an instance

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,5 +2,10 @@ import org.junit.Test;
 import static org.junit.Assert.
 
 public class HeroTest {
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+        Hero myHero = new Hero("Rashid",65,"cut","cut");
+        assertEquals(true, myHero instanceof Hero);
+    }
 }
 
