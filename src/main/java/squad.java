33416f9e234cdbd7b44bb472ed6852eb92a8a6e7
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-18 23:56:35 +0300

    declaring modifiers

--- src/main/java/squad.java
+++ src/main/java/squad.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class squad {
-}
+    private String mName;
+    private int mSize;
+    private String mCause;
+    private static List<squad> instances = new ArrayList<squad>();
+    private int mId;
+    private List<Hero> mHeroes;
+
+
