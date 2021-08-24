b913b4cbf39f71dc58b04de60dac6e770926a1d8
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-24 11:07:58 +0300

    generating getters and setters

--- src/main/java/avatar/Hero.java
+++ src/main/java/avatar/Hero.java
@@ -19,4 +19,36 @@ public class Hero {
         this.power = power;
         this.weakness = weakness;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
 }
