1190f3ebea5981fe5b2cd4971be728a97d6f45e2
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-24 11:04:07 +0300

    generating constructors

--- src/main/java/avatar/Hero.java
+++ src/main/java/avatar/Hero.java
@@ -13,4 +13,10 @@ public class Hero {
     private String squadUnion;
     private static boolean duplicate = false;
 
+    public Hero(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+    }
 }
