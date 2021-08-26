45a80b53b3f316ed2d4bbaf8ca88f2174123c371
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-26 08:18:56 +0300

    generating getters

--- src/main/java/avatar/Squad.java
+++ src/main/java/avatar/Squad.java
@@ -12,4 +12,24 @@ public class Squad {
     private int squadId;
     private boolean isSquadFull = false;
 
+    public Squad (String name, String cause Hero hero){
+        this.squadName = name.trim();
+        this.cause =cause.trim();
+        clone(hero.getHeroID());
+
+        if (isRegisteredHero){
+            hero.setsquadunion(squadName);
+            heroMembers.add(Hero);
+            squadList.add(hero);
+            this.squadId =squadList.size();
+        }else{
+            System.out.println("HERO IS NOT WITHIN OUR DATABASE");
+        }
+    }
+    public String getSquadName(){
+        return squadName;
+    }
+    public int getSquadId(){
+        return squadId;
+    }
 }
